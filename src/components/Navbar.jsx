e46bf42a6f2acdcf7445101be78dce10ec305399
Ahsan-Ayaz1 <ahsanayaz7755@gmail.com>
2023-08-13 01:45:19 +0500

    Show blogs in the home page useState

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <>
+            <nav className="navbar">
+                <h1>The Dojo Blog</h1>
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </nav>
+        </>
+    )
+}
